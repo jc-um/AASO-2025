91099cc6fc91e02c036a71af42d76d2f19a8239f
jc-um <jc240076@umconnect.umt.edu>
2025-05-29 16:05:01 -0600

    modified:   src/cleaner.py 	modified:   src/gui_manager.py

--- src/cleaner.py
+++ src/cleaner.py
@@ -10,11 +10,27 @@ class Cleaner():
         self._df[0] = pd.to_datetime(self._df[0], errors="coerce")
 
         self._df.dropna(inplace=True)
-        self._df.drop_duplicates(subset=[0])
+        self._df.drop_duplicates(subset=[0], inplace=True)
 
         n_col = self._df.columns[1:]
         self._df[n_col] = self._df[n_col].apply(pd.to_numeric, errors='coerce')
+        
+        self._df = self._df[5:].mul(0.6)
+        initial_data = self._df.iloc[0, 5:]
 
-        
-        
-        
+        s = self._df.shape
+        r = s[0]
+        c = s[1]
+
+        for i in range(5, c, 2):
+            for j in range(0, r):
+                initial_x = initial_data[i-1]
+                initial_y = initial_data[i]
+
+                curr_x = self._df.iloc[j, i-1]
+                curr_y = self._df.iloc[j, i]
+
+                displacement_x = curr_x - initial_x
+                displacement_y = curr_y - initial_y
+                
+                        
---
+++ src/gui_manager.py
@@ -0,0 +1,15 @@
+import cleaner as cln
+import tkinter as tk
+from tkinter import filedialog
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+
+class GuiManager():
+    def __init__(self):
+        pass
+    def plot_fig():
+        pass
+    def create_fig():
+        pass
+    
