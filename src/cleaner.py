01e91582ab5e2f0f382945f152dad35af872b9b5
jc-um <jc240076@umconnect.umt.edu>
2025-05-30 15:23:26 -0600

    modified:   requirements.txt 	new file:   src/clean.csv 	modified:   src/cleaner.py 	.gitignore

--- src/cleaner.py
+++ src/cleaner.py
@@ -12,7 +12,9 @@ class Cleaner():
     def clean_data(self, output_path="", max_magnitude=100):
         self._df[0] = pd.to_datetime(self._df[0], errors="coerce")
 
-        self._df.dropna(subset=[0], inplace=True)
+        self._df.replace("", float("NaN"), inplace=True)
+
+        self._df.dropna(how='all', axis=1, inplace=True)
         self._df.drop_duplicates(subset=[0], inplace=True)
 
         n_col = self._df.columns[1:]
@@ -22,39 +24,32 @@ class Cleaner():
         self._df = self._df.set_index(0).resample('h').ffill(limit=10).bfill(limit=10).reset_index()
         
         self._df[n_col] = self._df[n_col].interpolate(method="linear")
-        self._df = self._df.reset_index() 
+        #self._df = self._df.reset_index() 
 
-        
-        self._df.to_csv("./", index=False, header=False)  
-
-        self._df = self._df[5:].mul(0.6)
+        self._df = self._df.loc[:, 5:].mul(0.6)
         initial_data = self._df.iloc[0, 5:]
 
         s = self._df.shape
-        r = s[0]
-        c = s[1]
+        r = s[1]
+        c = s[0]
 
-        for i in range(5, c, 2):
+        for i in range(5, c):
             for j in range(0, r):
                 print(initial_data)
                 print(self._df.iloc[j, i])
-                initial_x = initial_data[i-1]
-                initial_y = initial_data[i]
+                initial = initial_data[i]
 
-                curr_x = self._df.iloc[j, i-1]
-                curr_y = self._df.iloc[j, i]
+                curr = self._df.iloc[j, i]
 
-                displacement_x = curr_x - initial_x
-                displacement_y = curr_y - initial_y
+                displacement = curr - initial
                 
-                self._df.iloc[j, i-1] = displacement_x
-                self._df.iloc[j, i] = displacement_y
+                self._df.iloc[j, i] = displacement
         
         if output_path == "":
             ext = self._path.rfind(".")
             output_path = self._path[:ext] + "_cleaned.csv"
             print(f"Saved to {output_path}")
-        self._df.to_csv(output_path, index=False, header=False)            
+        self._df.to_csv(output_path, index=False)            
 
 def main():
     data_cleaner = Cleaner(sys.argv[1])
